0f21d55d783d9ed9bf450f51020411e42b3faec0
Justin M <manningjustin08@yahoo.com>
2024-10-08 10:19:00 -0400

    just checked code

--- main.js
+++ main.js
@@ -3,54 +3,54 @@
 // starting with 20 plants
 // plants double in number
 
-let startingArea = 0.8 * 20;
-console.log(`The starting area of the plants is ${startingArea} square meters.`)
+// let startingArea = 0.8 * 20;
+// console.log(`The starting area of the plants is ${startingArea} square meters.`)
 
-let week2 = 0.8 * 40 
+// let week2 = 0.8 * 40 
 
-console.log(`The area of the plants in week one is ${week2} square meters.`)
+// console.log(`The area of the plants in week one is ${week2} square meters.`)
 
 
 
-    function weeksSpace(weeks){  
+//     function weeksSpace(weeks){  
         
-        const spaceNeeded = 0.8;
-        let amountOfPlants = 20;
-        const maxCapac = 78.5375;
+//         const spaceNeeded = 0.8;
+//         let amountOfPlants = 20;
+//         const maxCapac = 78.5375;
     
-       // doubles over time
-        let totalPlants = amountOfPlants * Math.pow(2, weeks);
+//        // doubles over time
+//         let totalPlants = amountOfPlants * Math.pow(2, weeks);
     
     
-        let totalSpaceRequired = totalPlants * spaceNeeded;
+//         let totalSpaceRequired = totalPlants * spaceNeeded;
 
-        console.log(totalSpaceRequired)
+//         console.log(totalSpaceRequired)
 
-        if (totalSpaceRequired > (maxCapac * 0.8)){
-            console.log("Prune")
-        } else if (totalSpaceRequired > (0.5 * maxCapac) && totalSpaceRequired < (0.8 * maxCapac)){
-            console.log("Monitor")
-        } else{
-            console.log("Plant More")
-        }
+//         if (totalSpaceRequired > (maxCapac * 0.8)){
+//             console.log("Prune")
+//         } else if (totalSpaceRequired > (0.5 * maxCapac) && totalSpaceRequired < (0.8 * maxCapac)){
+//             console.log("Monitor")
+//         } else{
+//             console.log("Plant More")
+//         }
 
-        console.log(`After ${weeks} weeks, the total space required is ${totalSpaceRequired.toFixed(2)} square meters.`);
-        }
-
-
-       function hry(weeks){
-             let hundredPlants = 100 + Math.pow(2, weeks);
-        let hundredRadius = Math.sqrt(hundredPlants/3.1415)
-        console.log(`The area of the plants after 10 weeks is ${hundredPlants} square meters. The radius would be ${hundredRadius}.`)
-
-        }
+//         console.log(`After ${weeks} weeks, the total space required is ${totalSpaceRequired.toFixed(2)} square meters.`);
+//         }
 
 
-    // weeksSpace(1);
-    // weeksSpace(2);
-    // weeksSpace(3);
-        hry(10);
-    // weeksSpace(10);
+//        function hry(weeks){
+//              let hundredPlants = 100 + Math.pow(2, weeks);
+//         let hundredRadius = Math.sqrt(hundredPlants/3.1415)
+//         console.log(`The area of the plants after 10 weeks is ${hundredPlants} square meters. The radius would be ${hundredRadius}.`)
+
+//         }
+
+
+//     // weeksSpace(1);
+//     // weeksSpace(2);
+//     // weeksSpace(3);
+//         hry(10);
+//     // weeksSpace(10);
 
 
       
